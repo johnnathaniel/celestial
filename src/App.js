256030e8f87e40f510313201ab6105960b0eb022
John Cohoon <100147+johnnathaniel@users.noreply.github.com>
2023-06-11 11:06:15 -0700

    star wars planets demo project

--- src/App.js
+++ src/App.js
@@ -1,23 +1,69 @@
-import logo from './logo.svg';
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
 import './App.css';
 
-function App() {
+const App = () => {
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(false);
+  const [planets, setPlanets] = useState([]);
+
+  useEffect(() => {
+    setLoading(true);
+    axios.get('https://swapi.dev/api/planets/')
+      .then(response => {
+        const sortedPlanets = response.data.results.sort((a, b) => a.name.localeCompare(b.name));
+        setPlanets(sortedPlanets);
+        setLoading(false);
+      })
+      .catch(() => {
+        setLoading(false);
+        setError(true);
+      });
+  }, []);
+
+  const formatNumber = (num) => {
+    if (num === 'unknown') return '?';
+    return parseInt(num).toString().replace(/\B(?=(\d{3})+(?!\d))/g, ' ');
+  }
+  
+  const calcSurfaceWaterArea = (diameter, surfaceWater) => {
+    if (diameter === 'unknown' || surfaceWater === 'unknown') return '?';
+    const radius = parseInt(diameter) / 2;
+    const totalSurfaceArea = 4 * Math.PI * Math.pow(radius, 2);
+    const waterArea = totalSurfaceArea * (parseInt(surfaceWater) / 100);
+    return formatNumber(Math.round(waterArea));
+  }
+  
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Star Wars Planets</h1>
+      {loading && <p>Loading...</p>}
+      {error && <p>An error occurred. Please try again later.</p>}
+      <table className="planets-table">
+        <thead>
+          <tr>
+            <th>Name</th>
+            <th>Climate</th>
+            <th>Residents</th>
+            <th>Terrain</th>
+            <th>Population</th>
+            <th>Surface Water Area (km²)</th>
+          </tr>
+        </thead>
+        <tbody>
+          {planets.map((planet, index) => (
+            <tr key={index}>
+              <td><a href={planet.url} target="_blank" rel="noreferrer">{planet.name}</a></td>
+              <td>{planet.climate}</td>
+              <td>{planet.residents.length}</td>
+              <td>{planet.terrain}</td>
+              <td>{formatNumber(planet.population)}</td>
+              <td>{calcSurfaceWaterArea(planet.diameter, planet.surface_water)}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
     </div>
   );
 }
